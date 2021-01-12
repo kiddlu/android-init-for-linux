1f4834ef78d2a2ab78e77482916df2c3bd95b2b9
kiddlu <kidd.dawny.lu@gmail.com>
2021-01-12 12:45:13 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

--- init/path.h
+++ init/path.h
@@ -1,4 +1,4 @@
-#define ROOTDIR                   "/tmp/rootdir/"
+#define ROOTDIR                   "/platform/"
 #define INITRC_FILE_PATH          ROOTDIR"/init.rc"
-#define PERSISTENT_PROPERTY_DIR   ROOTDIR"/data/property"
-#define PROP_PATH_SYSTEM_DEFAULT  ROOTDIR"/data/default.prop"
+#define PERSISTENT_PROPERTY_DIR   ROOTDIR"/property/"
+#define PROP_PATH_SYSTEM_DEFAULT  ROOTDIR"/property/default.prop"
